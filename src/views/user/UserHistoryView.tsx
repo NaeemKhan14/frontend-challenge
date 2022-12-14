e6d628a82bebd6e9fbf00a75c84b1fd20983395d
Naeem Khan <n.khan@archirodon.net>
2022-12-14 16:04:27 +0400

    Completed history table

--- src/views/user/UserHistoryView.tsx
+++ src/views/user/UserHistoryView.tsx
@@ -1,5 +1,53 @@
 import { ReactElement } from 'react';
+import history_data from '../../../dummyData.json';
 
 export const UserHistoryView = (): ReactElement => {
-  return <div>{'I could be a table'}</div>;
+  // Prepare table css style
+  const styles = `
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    
+    td, th {
+        border: 3px solid #eee;
+        text-align: center;
+        padding: 10px;
+    }
+    
+    tr:nth-child(even) {
+        background-color: #eee;
+    }
+    `;
+  // Prepare data from the json object
+  const displayHistoryData = history_data.HISTORY_DATA.map((data) => {
+    return (
+      <tr>
+        <td>{data.year}</td>
+        <td>{data.month}</td>
+        <td>{data.co2_saved}</td>
+        <td>{data.reward}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Month</th>
+            <th>Co2 Saved</th>
+            <th>Reward</th>
+          </tr>
+        </thead>
+        {/* Call the previously prepared data */}
+        <tbody>{displayHistoryData}</tbody>
+      </table>
+      <style>{styles}</style>
+    </div>
+  );
 };
+
+export default UserHistoryView;
